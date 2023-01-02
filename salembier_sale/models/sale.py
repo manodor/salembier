063c9dcff4903560ecebf5ba813f70b8c126471e
Abdelhalim <hakimguettaoui@gmail.com>
2023-01-02 16:27:49 +0100

    [sale] calcul the total of qty to set the price based in grouping family

--- salembier_sale/models/sale.py
+++ salembier_sale/models/sale.py
@@ -36,93 +36,63 @@ class SaleOrder(models.Model):
                 if available_qty < line.product_uom_qty:
                     raise UserError(_("Make sure you have the quantities available in stock (obsolete product): %s",
                                       line.product_id.name))
+    """Code for the grouping family total price """
+    def _get_search_vals_for_quantity(self, line):
+        return [
+            ('order_id', '=', line.order_id.id),
+            ('state', '!=', 'cancel'),
+            ('product_id.categ_family_gruping', '=', line.product_id.categ_family_gruping.id),
+        ]
+
+    def _get_quantity_to_compute(self, line):
+        quantity = line.product_uom_qty
+        if line.product_id:
+            order_line_obj = self.env['sale.order.line']
+            search_domain = self._get_search_vals_for_quantity(line)
+            line_ids = order_line_obj.search(search_domain)
+            quantity = sum([x.product_uom_qty for x in line_ids])
+        return quantity
+
+    def _get_price_of_line(self, product_id,
+                           qty, partner_id, pricelist_id):
+        return pricelist_id._compute_price_rule_multi(
+            products_qty_partner=[(product_id, qty, partner_id)])
+
+    def _check_if_edit(self, res, product_id):
+        if res.get(product_id):
+            vals = res.get(product_id, False)
+            # get the 2nd item of the tuple
+            item_id = vals[2][1]
+            if item_id:
+                item = self.env['product.pricelist.item'].browse(item_id)
+                if item.grouping_family_id:
+                    return True
+        return False
+
+    def _get_child_pricelist(self, res):
+        if res.get('item_id'):
+            item_id = res.get('item_id', False)
+            if item_id:
+                item = self.env['product.pricelist.item'].browse(item_id)
+                if item.base_pricelist_id:
+                    return item.base_pricelist_id
+        return False
+
+    def compute_global_discount(self):
+        for sale in self:
+            if sale.state in ['draft', 'sent']:
+                sale.order_line.compute_global_discount()
+        return True
 
     @api.model
     def create(self, values):
-
         res = super(SaleOrder, self).create(values)
-
-        grouping_family_qty = {}
-        grouping_family_product = {}
-        for line in res.order_line:
-            if line.display_type not in ('line_section', 'line_note'):
-                if line.product_id.categ_family_gruping.id in grouping_family_product:
-                    grouping_family_qty[line.product_id.categ_family_gruping.id] += line.product_uom_qty
-                else:
-                    grouping_family_qty[line.product_id.categ_family_gruping.id] = line.product_uom_qty
-                    grouping_family_product[line.product_id.categ_family_gruping.id] = line.product_id
-
-        for record in grouping_family_product:
-            for line in res.order_line:
-                if line.display_type not in ('line_section', 'line_note'):
-                    qty_save = line.product_uom_qty
-                    line.product_uom_qty = grouping_family_qty[record]
-
-                    if line.order_id.pricelist_id and line.order_id.partner_id:
-                        product = line.product_id.with_context(
-                            lang=line.order_id.partner_id.lang,
-                            partner=line.order_id.partner_id,
-                            quantity=grouping_family_qty[record],
-                            date=line.order_id.date_order,
-                            pricelist=line.order_id.pricelist_id.id,
-                            uom=line.product_uom.id,
-                            fiscal_position=self.env.context.get('fiscal_position')
-                        )
-                        line.price_unit = product._get_tax_included_unit_price(
-                            line.company_id or line.order_id.company_id,
-                            line.order_id.currency_id,
-                            line.order_id.date_order,
-                            'sale',
-                            fiscal_position=line.order_id.fiscal_position_id,
-                            product_price_unit=line._get_display_price(product),
-                            product_currency=line.order_id.currency_id
-                        )
-
-                    line.product_uom_qty = qty_save
-
+        res.compute_global_discount()
         return res
 
     def write(self, values):
-
         res = super(SaleOrder, self).write(values)
-
-        grouping_family_qty = {}
-        grouping_family_product = {}
-        for line in self.order_line:
-            if line.display_type not in ('line_section', 'line_note'):
-                if line.product_id.categ_family_gruping.id in grouping_family_product:
-                    grouping_family_qty[line.product_id.categ_family_gruping.id] += line.product_uom_qty
-                else:
-                    grouping_family_qty[line.product_id.categ_family_gruping.id] = line.product_uom_qty
-                    grouping_family_product[line.product_id.categ_family_gruping.id] = line.product_id
-
-        for record in grouping_family_product:
-            for line in self.order_line:
-                if line.display_type not in ('line_section', 'line_note'):
-                    qty_save = line.product_uom_qty
-                    line.product_uom_qty = grouping_family_qty[record]
-
-                    if line.order_id.pricelist_id and line.order_id.partner_id:
-                        product = line.product_id.with_context(
-                            lang=line.order_id.partner_id.lang,
-                            partner=line.order_id.partner_id,
-                            quantity=grouping_family_qty[record],
-                            date=line.order_id.date_order,
-                            pricelist=line.order_id.pricelist_id.id,
-                            uom=line.product_uom.id,
-                            fiscal_position=self.env.context.get('fiscal_position')
-                        )
-                        line.price_unit = product._get_tax_included_unit_price(
-                            line.company_id or line.order_id.company_id,
-                            line.order_id.currency_id,
-                            line.order_id.date_order,
-                            'sale',
-                            fiscal_position=line.order_id.fiscal_position_id,
-                            product_price_unit=line.price_unit,
-                            product_currency=line.order_id.currency_id
-                        )
-
-                    line.product_uom_qty = qty_save
+        self.compute_global_discount()
         return res
 
 
@@ -177,3 +147,36 @@ class SaleOrderLine(models.Model):
         elif self.product_id.product_tmpl_id:
             name = self.product_id.product_tmpl_id.name
         self.update({'name': name})
+
+    def compute_global_discount(self):
+        for line in self:
+            sale = line.order_id.with_context(date=line.order_id.date_order)
+            partner_id = sale.partner_id.id
+            if line.product_id:
+                pricelist_id = sale.pricelist_id
+                product_id = line.product_id
+                qty = sale._get_quantity_to_compute(line)
+                if qty:
+                    res = sale._get_price_of_line(
+                        product_id, qty, partner_id, pricelist_id)
+                    while True:
+                        new_pricelist_id = sale._get_child_pricelist(res)
+                        if not new_pricelist_id:
+                            break
+                        res = sale._get_price_of_line(product_id,
+                                                      qty, partner_id,
+                                                      new_pricelist_id)
+                        pricelist_id = new_pricelist_id
+                    price_unit = False
+                    if sale._check_if_edit(res, line.product_id.id):
+                        price_unit = res.get(
+                            line.product_id.id)[2][0]
+                        print(price_unit)
+                    else:
+                        price_unit = pricelist_id.with_context(
+                            date=line.order_id.date_order).price_get(
+                            product_id.id, line.product_uom_qty,
+                            partner=sale.partner_id.id)[pricelist_id.id]
+                    if price_unit is not False:
+                        line.write({'price_unit': price_unit})
+        return True
