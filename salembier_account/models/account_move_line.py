b89938af6634282a1d63e03af7fcd762d2eaf85c
Abdelhalim <hakimguettaoui@gmail.com>
2023-01-04 17:00:46 +0100

    [account] change the fields type of serial number

--- salembier_account/models/account_move_line.py
+++ salembier_account/models/account_move_line.py
@@ -7,3 +7,4 @@ class AccountMove(models.Model):
     _inherit = "account.move.line"
 
     account_serial_number = fields.Char(string='Serial Number', required=False, store=True)
+    account_serial_number_html = fields.Html(string='Serial Number', required=False, store=True)
