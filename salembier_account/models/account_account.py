9dc2cb40db7b9a9d61e3f267bc29125f2880ab4c
Abdelhalim <hakimguettaoui@gmail.com>
2023-01-17 13:23:10 +0100

    [account] delete all account with 6 value

--- salembier_account/models/account_account.py
+++ salembier_account/models/account_account.py
@@ -13,3 +13,11 @@ class AccountAccount(models.Model):
                 code = rec.code + '00'
                 if code not in account.mapped('code'):
                     rec.code = code
+
+    def delete_double_account(self):
+        account = self.env['account.account'].search([])
+        for rec in account:
+            # find if the account is already used in account.move.line
+            move_line = self.env['account.move.line'].search([('account_id', '=', rec.id)])
+            if len(rec.code) == 6 and rec.group_id and rec.group_id.id not in [87,82] and not move_line:
+                rec.unlink()
